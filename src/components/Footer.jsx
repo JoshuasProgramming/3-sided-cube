09ab0b6c45f6ff5d64fbe07b53446a08d0efbdf2
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-10-21 13:59:31 +0100

    Added images for footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+//react imports
+import React, { Component } from 'react';
+
+// import images / logos
+import Twitter from '../images/Twitter.png';
+import Instagram from '../images/Instagram.png';
+import Facebook from '../images/Facebook.png';
+import LinkedIn from '../images/LinkedIn.png';
+import YouTube from '../images/YouTube.png';
+
+class Footer extends Component {
+    
+    render(){
+        return (
+            <footer>
+
+                {/* Row 1 */}
+
+
+                {/* Row 2 */}
+
+                {/* Row 3 */}
+
+            </footer>
+        )
+    }
+
+}
+
+export default Footer
