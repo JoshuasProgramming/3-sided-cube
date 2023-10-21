edbd0ff0f782ab1668e33e37449686584f3c184f
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-10-21 14:07:59 +0100

    Made footer stick to the bottom of the screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 
 import Navbar from '../src/components/Navbar';
-
+import Footer from '../src/components/Footer';
 
 function App() {
   return (
-    <>
+    <div className='flex flex-col h-screen'>
       <Navbar/>
-    </>
+      <Footer/>
+    </div>
   );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,14 +8,18 @@ import Facebook from '../images/Facebook.png';
 import LinkedIn from '../images/LinkedIn.png';
 import YouTube from '../images/YouTube.png';
 
+import Company_Logo from '../images/logo.png';
+import Text_Logo from '../images/text_logo.png';
+
 class Footer extends Component {
     
     render(){
         return (
-            <footer>
-
+            <footer className='bg-black border-gray-200 dark:bg-grey-900 flex items-center justify-between flex-row h-48 mt-auto'>
+                <p>test</p>
                 {/* Row 1 */}
 
+                
 
                 {/* Row 2 */}
 
